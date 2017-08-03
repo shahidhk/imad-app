51dd3e68cfc3ede84c41a2c1a89629bacbafffa4
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2017-08-03 10:22:48 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: 'test-user-1'
 };
 
 var Pool = require('pg').Pool;
