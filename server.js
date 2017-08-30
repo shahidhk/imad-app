1299fc5eceb35e9a00ae9addc4e48be37ed0d8cd
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2017-08-30 15:35:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-
-var config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-};
-
-var Pool = require('pg').Pool;
-var pool = new Pool(config);
-
 var app = express();
 app.use(morgan('combined'));
 
@@ -28,18 +17,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/dbtest', function(req, res) {
-    console.log('start');
-    pool.query('SELECT * FROM test', function (err, result){
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    });
-    console.log('end');
-    
-});
 
 var port = 80; // Use 8080 for local development because you might already have apache running on 80
 app.listen(port, function () {
