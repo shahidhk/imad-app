ec8a8860bd9fe19390c782ec17397b7ed98bd9e3
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2018-02-26 13:58:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,57 +1,25 @@
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
+var config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+};
+
+var Pool = require('pg').Pool;
+var pool = new Pool(config);
+
 var app = express();
 app.use(morgan('combined'));
 
-
-function createTemplate (data) {
-    var title = data.title;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = `
-        <html>
-          <head>
-              <title>
-                  ${title}
-              </title>
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link href="/ui/style.css" rel="stylesheet" />
-          </head> 
-          <body>
-              <div class="container">
-                  <div>
-                      <a href="/">Home</a>
-                  </div>
-                  <hr/>
-                  <h3>
-                      ${heading}
-                  </h3>
-                  <div>
-                    ${content}
-                  </div>
-              </div>
-          </body>
-        </html>
-        `;
-    return htmlTemplate;
-}
-    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/article-one.html', function (req, res) {
-    var data = {
-    "title": "Article 1",
-    "heading" : "Article 1 heading",
-    "content": "Article 1 content"
-};
-
-  res.send(data);
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -61,6 +29,18 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/dbtest', function(req, res) {
+    console.log('start');
+    pool.query('SELECT * FROM test', function (err, result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+    console.log('end');
+    
+});
 
 var port = 80; // Use 8080 for local development because you might already have apache running on 80
 app.listen(port, function () {
